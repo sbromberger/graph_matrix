b0a26cbe1bee8e3595a6d04df6b4c72f4c59da58
Seth Bromberger <github@bromberger.com>
2020-04-20 14:35:04 -0700

    first

---
+++ src/lib.rs
@@ -0,0 +1,183 @@
+use num;
+use std::fmt;
+
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
+use std::path::Path;
+
+pub trait MxElement: num::PrimInt + std::fmt::Debug {}
+impl MxElement for u8 {}
+impl MxElement for u16 {}
+impl MxElement for u32 {}
+impl MxElement for u64 {}
+
+fn search_sorted_iter<T>(it: std::slice::Iter<T>, n: T) -> Option<usize>
+where
+    T: MxElement,
+{
+    for (i, el) in it.enumerate() {
+        if *el == n {
+            return Some(i);
+        }
+        if *el > n {
+            return None;
+        }
+    }
+    None
+}
+
+fn compress<T>(row: Vec<T>, col: Vec<T>, n: usize) -> (Vec<usize>, Vec<T>)
+where
+    T: MxElement,
+{
+    // println!("n = {}", n);
+    let mut w: Vec<usize> = vec![0; n];
+    let mut ja: Vec<T> = vec![T::zero(); col.len()];
+
+    // println!("length of w = {}", w.len());
+    for v in &row {
+        // println!("setting w[{:?}]", v);
+        w[v.to_usize().expect("Invalid vertex entry")] += 1;
+    }
+    let ia = w.iter().fold(vec![0], |mut acc, val| {
+        acc.push(val + acc.last().unwrap());
+        acc
+    });
+    let mut w = ia.clone();
+    if let Some(last) = w.last_mut() {
+        *last = 0;
+    }
+    // println!("w = {:?}", w);
+    // println!("ia = {:?}", ia);
+    for (j, v) in col.into_iter().enumerate() {
+        let rj = row[j].to_usize().expect("Invalid vertex entry");
+        let p = w[rj];
+        // println!("rj = {}, p = {}, w = {:?}", rj, p, w);
+        ja[p] = v;
+        w[rj] += 1;
+    }
+    // println!("w = {:?}", w);
+
+    (ia, ja)
+}
+
+#[derive(Debug, Clone)]
+pub struct GraphMatrix<T>
+where
+    T: MxElement,
+{
+    indptr: Vec<usize>,
+    indices: Vec<T>,
+}
+
+impl<T> GraphMatrix<T>
+where
+    T: MxElement,
+{
+    pub fn from_edges(edgelist: Vec<(T, T)>) -> Self {
+        // -> GraphMatrix<T, U>  {
+        // assume edgelist is not sorted
+        let mut sorted_edge_list = edgelist;
+        sorted_edge_list.sort_unstable();
+        let (ss, ds): (Vec<_>, Vec<_>) = sorted_edge_list.into_iter().unzip();
+
+        // println!("ss = {:?}, ds = {:?}", ss, ds);
+        let m1 = ss.last().expect("Edgelist must not be empty");
+        let m2 = ds.iter().max().expect("Edgelist must not be empty");
+        let m = m1
+            .max(m2)
+            .to_usize()
+            .expect("Number of vertices must be less than 2^64-2")
+            + 1;
+        // println!("m1 = {:?}, m2 = {:?}, m = {}", m1, m2, m);
+        let (indptr, indices) = compress(ss, ds, m);
+        GraphMatrix { indptr, indices }
+    }
+    // we don't want to consume self, and we need to ensure that the iterator lasts as long as the
+    // struct. Shorthand for this is pub fn row(&self, r: usize) -> impl Iterator<Item = T> + '_.
+    // pub fn row<'a>(&'a self, r: usize) -> impl Iterator<Item = T> + 'a
+    pub fn row<'a>(&'a self, r: usize) -> std::slice::Iter<T>
+    where
+        T: MxElement,
+    {
+        if r > self.indptr.len() - 1 {
+            panic!("Row {} is out of bounds (max {})", r, self.indptr.len() - 1)
+        }
+        let row_start = self.indptr[r];
+        let row_end = self.indptr[r + 1];
+        self.indices[row_start..row_end].into_iter()
+    }
+
+    pub fn row_len(&self, r: usize) -> T
+    where
+        T: MxElement,
+    {
+        if r > self.indptr.len() - 1 {
+            panic!("Row {} is out of bounds (max {})", r, self.indptr.len() - 1)
+        }
+        let row_start = self.indptr[r];
+        let row_end = self.indptr[r + 1];
+        T::from(row_end - row_start).expect("Something went wrong.")
+    }
+
+    pub fn has_index(&self, r: usize, c: usize) -> bool
+    where
+        T: MxElement,
+    {
+        let row = self.row(r);
+        let tc = T::from(c).expect("invalid vertex");
+        search_sorted_iter(row, tc).is_some()
+    }
+
+    pub fn from_edge_file(fname: &Path) -> Self {
+        let f = File::open(fname).expect("Cannot open file");
+        let file = BufReader::new(&f);
+        let mut edgelist: Vec<(T, T)> = vec![];
+        for line in file.lines() {
+            let l = line.expect("error reading file"); // produces a std::string::String
+            let l = l.trim(); // changes to &str
+            if l.starts_with("#") {
+                continue;
+            }
+            let mut eit = l.split_whitespace();
+            let s1 = eit.next().expect("Invalid line (first field)");
+            let s2 = eit.next().expect("Invalid line (second field)");
+            if eit.next().is_some() {
+                panic!("Invalid line (extra fields)");
+            }
+
+            let src128: u128 = s1.parse().unwrap();
+            let dst128: u128 = s2.parse().unwrap();
+
+            let src = T::from(src128).expect("vertex out of range");
+            let dst = T::from(dst128).expect("vertex out of range");
+            edgelist.push((src, dst));
+        }
+        GraphMatrix::from_edges(edgelist)
+    }
+
+    // return the number of defined values
+    pub fn n(&self) -> usize {
+        self.indices.len()
+    }
+
+    pub fn dim(&self) -> usize {
+        self.indptr.len() - 1
+    }
+}
+
+impl<T> fmt::Display for GraphMatrix<T>
+where
+    T: MxElement,
+{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "GraphMatrix {} x {} with {} entries",
+            self.dim(),
+            self.dim(),
+            self.n()
+        )
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use graph_matrix::GraphMatrix;
+use std::env;
+use std::path::Path;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let filename = &args[1];
+
+    let edges = vec![(0u32, 1u32), (1u32, 2u32), (2u32, 3u32), (1u32, 3u32)];
+    let g = GraphMatrix::from_edges(edges);
+    println!("g = {}", g);
+    let h: GraphMatrix<u32> = GraphMatrix::from_edge_file(Path::new(filename));
+    println!("h = {}", h);
+}
